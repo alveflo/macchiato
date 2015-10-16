e0a615a273ec92c488b2b1aa1ea618a92f315e71
Victor Alveflo <victor.alveflo@gmail.com>
2015-10-17 00:16:24 +0200

    Fixed login/signup issue

--- tasks/paths.js
+++ tasks/paths.js
@@ -1,21 +1,21 @@
 module.exports = {
   'client': {
-    'views':     'client/views/**/*.jade',
-    'style':   'client/style/**/*.styl',
-    'js':       'client/js/*.js',
-    'jsng':     'client/js/ng/**/*.js',
-    'img': 'client/img/**/*.png',
-    'semantic': 'bower_components/semantic-ui/dist/**/*'
+    'views':              'client/views/**/*.jade',
+    'style':              'client/style/**/*.styl',
+    'js':                 'client/js/*.js',
+    'jsng':               'client/js/ng/**/*.js',
+    'img':                'client/img/**/*.png',
+    'semantic':           'bower_components/semantic-ui/dist/**/*'
   },
   'build': {
     'public': {
-      'img': 'app/public/img',
-      'js': 'app/public/js',
-      'assets': 'app/public/assets',
-      'assets_semantic': 'app/public/assets/semanticui',
-      'style': 'app/public/style',
+      'img':              'app/public/img',
+      'js':               'app/public/js',
+      'assets':           'app/public/assets',
+      'assets_semantic':  'app/public/assets/semanticui',
+      'style':            'app/public/style',
     },
-    'public_dir': 'app/public',
-    'views': 'app/views'
+    'public_dir':         'app/public',
+    'views':              'app/views'
   }
 }
