65bc621e13b571e67da19609c74dc517ef88412e
Victor Alveflo <victor.alveflo@gmail.com>
2015-10-25 21:59:07 +0100

    Removed clean-urls dependency

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var cleanUrls = require('clean-urls');
 
 var app = express();
 // Passport setup
